12901bcf5f0cca16585bd3c0d4e708a8c390384e
vaofford <vo1@sanger.ac.uk>
2020-07-10 11:44:12 +0000

    Update base image to R 4.x.x

--- build/install_R_packages.sh
+++ build/install_R_packages.sh
@@ -1,7 +1,22 @@
 #! /bin/bash
 
-Rscript -e "install.packages(pkgs = c('devtools', 'tidyverse', 'argparse', 'pheatmap', 'optparse', 'viridis', 'extrafont', 'rdgal', 'ggrepel', 'showtext'), repos='https://www.stats.bris.ac.uk/R/', dependencies=TRUE, clean = TRUE)"
-Rscript -e "install.packages(pkgs = c('hrbrthemes'), repos = 'https://cinc.rud.is', dependencies=TRUE, clean = TRUE)"
+Rscript -e "install.packages( pkgs = c( 'devtools', 
+					'tidyverse', 
+					'argparse', 
+					'optparse', 
+					'pheatmap',
+					'viridis',
+					'extrafont',
+					'rdgal',
+					'ggrepel',
+					'showtext',
+					'GGally',
+					'jsonlite',
+					'RColorBrewer' ), 
+				repos = 'https://www.stats.bris.ac.uk/R/', 
+				dependencies = TRUE, 
+				clean = TRUE )"
 
-Rscript -e "install.packages('BiocManager', repos='https://www.stats.bris.ac.uk/R/')"
-Rscript -e "BiocManager::install()"
+Rscript -e "install.packages( pkgs = c( 'hrbrthemes' ), repos = 'https://cinc.rud.is', dependencies = TRUE, clean = TRUE )"
+Rscript -e "install.packages( 'BiocManager', repos = 'https://www.stats.bris.ac.uk/R/' )"
+Rscript -e "BiocManager::install( c( 'ensembldb', 'GenomicRanges', 'rhdf5' ) )"

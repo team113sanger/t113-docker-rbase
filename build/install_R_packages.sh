e86433f4183fd233e9bb47a3204262cdcfbefc5d
vo1 <vo1@sanger.ac.uk>
2020-07-14 20:28:58 +0000

    avoid travis timeout

--- build/install_R_packages.sh
+++ build/install_R_packages.sh
@@ -1,6 +1,13 @@
 #! /bin/bash
 
-Rscript -e "install.packages( pkgs = c( 'devtools', 'tidyverse', 'argparse', 'optparse', 'pheatmap', 'viridis', 'extrafont', 'rdgal', 'ggrepel', 'showtext', 'GGally', 'jsonlite', 'RColorBrewer', 'futile.logger' ), repos = 'https://www.stats.bris.ac.uk/R/', dependencies = TRUE, clean = TRUE, quiet = TRUE )"
+Rpackages=( 'devtools' 'tidyverse' 'argparse' 'optparse' 'pheatmap' 'viridis' 'extrafont' 'rdgal' 'ggrepel' 'showtext' 'GGally' 'jsonlite' 'RColorBrewer' 'futile.logger' )
+
+for pkg in ${Rpackages[@]}; do
+  echo "Installing $pkg"
+  Rscript -e "install.packages( pkgs = c( '${pkg}' ), repos = 'https://www.stats.bris.ac.uk/R/', dependencies = TRUE, clean = TRUE, quiet = TRUE )"
+done
+
+#Rscript -e "install.packages( pkgs = c( 'devtools', 'tidyverse', 'argparse', 'optparse', 'pheatmap', 'viridis', 'extrafont', 'rdgal', 'ggrepel', 'showtext', 'GGally', 'jsonlite', 'RColorBrewer', 'futile.logger' ), repos = 'https://www.stats.bris.ac.uk/R/', dependencies = TRUE, clean = TRUE, quiet = TRUE )"
 Rscript -e "install.packages( pkgs = c( 'hrbrthemes' ), repos = 'https://cinc.rud.is', dependencies = TRUE, clean = TRUE, quiet = TRUE )"
 Rscript -e "install.packages( 'BiocManager', repos = 'https://www.stats.bris.ac.uk/R/', quiet = TRUE )"
 Rscript -e "BiocManager::install( c( 'ensembldb', 'GenomicRanges', 'rhdf5' ), quiet = TRUE )"
